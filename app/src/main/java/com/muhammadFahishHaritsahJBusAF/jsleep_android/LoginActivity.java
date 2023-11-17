3170348f3112693f7b007cd37c2583356d4a9424
Harrist Muhammad <harristfhs@gmail.com>
2023-11-17 17:58:57 +0700

    CS08 - Completing CS08

--- app/src/main/java/com/muhammadFahishHaritsahJBusAF/jsleep_android/LoginActivity.java
+++ app/src/main/java/com/muhammadFahishHaritsahJBusAF/jsleep_android/LoginActivity.java
@@ -2,13 +2,45 @@ package com.muhammadFahishHaritsahJBusAF.jsleep_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView registerNow = null;
+    private Button loginButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerNow = findViewById(R.id.register_now);
+        loginButton = findViewById(R.id.login_button);
+
+        registerNow.setOnClickListener(e -> {
+            moveActivity(this, RegisterActivity.class);
+        });
+
+        loginButton.setOnClickListener(e -> {
+            moveActivity(this, MainActivity.class);
+        });
+
+        getSupportActionBar().hide();
     }
+
+    private void moveActivity(Context context, Class<?> cls) {
+        Intent intent = new Intent(context, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
---
+++ app/src/main/java/com/muhammadFahishHaritsahJBusAF/jsleep_android/MainActivity.java
@@ -0,0 +1,23 @@
+
+package com.muhammadFahishHaritsahJBusAF.jsleep_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+
+        return true;
+    }
+}
